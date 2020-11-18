687993ee47644ca7191f340bdbd180b4164a9ba3
Maeve03 <elkyie0309@gmail.com>
2020-11-18 12:03:32 +0530

    adding things

---
+++ build/precache-manifest.57f55ca2e4e587e0467ca3bd78b43dd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9257a6d5cef3c0affb4bf66759bb0a78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fecfd626dc1da629d3e",
+    "url": "/static/css/main.c843d8ba.chunk.css"
+  },
+  {
+    "revision": "405639461ca7a03bf645",
+    "url": "/static/js/2.e65422a2.chunk.js"
+  },
+  {
+    "revision": "c80ac9a9381e852bfe3f140fb667e2e6",
+    "url": "/static/js/2.e65422a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fecfd626dc1da629d3e",
+    "url": "/static/js/main.692215eb.chunk.js"
+  },
+  {
+    "revision": "f0e9e0561a1081db1f31",
+    "url": "/static/js/runtime-main.0a541afb.js"
+  }
+]);
